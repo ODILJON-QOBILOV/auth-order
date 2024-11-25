fd05ab0c11e77c939d3e71c35d5094b20a134300
QOBILOV-ODILJON <qobilovodiljonoff@gmail.com>
2024-11-25 21:13:39 +0500

    final commit

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from users.views import RegisterAPIView, Test, LoginAPIView, RefreshTokenAPIView
+from users.views import RegisterAPIView, LoginAPIView, RefreshTokenAPIView
 
 urlpatterns = [
     path('register/', RegisterAPIView.as_view()),

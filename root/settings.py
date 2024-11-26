5692ee5e51840ce1114ca68f9c99a917d1a8c8e1
QOBILOV-ODILJON <qobilovodiljonoff@gmail.com>
2024-11-26 16:47:14 +0500

    final commit

--- root/settings.py
+++ root/settings.py
@@ -30,7 +30,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'rest_framework',
+    'rest_framework.authtoken',
     'corsheaders',
     'rest_framework_simplejwt.token_blacklist',
 
